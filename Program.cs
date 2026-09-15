8a7628575a9b257e13644d84fc448fb894cc9459
beloyarova <abeloarova@gmail.com>
2026-09-15 18:28:14 +0300

    Lab01:first program with Environment

--- Program.cs
+++ Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
+Console.WriteLine($"Пользователь: {Environment.UserName}");
+Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
